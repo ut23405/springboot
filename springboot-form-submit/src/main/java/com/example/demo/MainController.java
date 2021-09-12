de4b0dd0556e0d3eddc5c73f131a3f7d3cf498ae
ut23405 <ut23405@gmail.com>
2021-09-12 22:57:34 +0900

    form-submit

--- springboot-form-submit/src/main/java/com/example/demo/MainController.java
+++ springboot-form-submit/src/main/java/com/example/demo/MainController.java
@@ -2,19 +2,17 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/test1")
 public class MainController {
 	
-	@GetMapping
+	@GetMapping("/test1")
     public String input1() {
         return "test1/index";
     }
 
-	@GetMapping("testform")
+	@GetMapping("/testform")
 	public String output1(
 		@RequestParam(name = "text1") String str1,
 		Model model) {
