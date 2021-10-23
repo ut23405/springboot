7ae0fd3aa905c2b718efb3cbded7e02fc955c4d9
ut23405 <ut23405@gmail.com>
2021-10-24 02:37:58 +0900

    error404

--- springboot-error404/src/main/java/com/example/demo/MainController.java
+++ springboot-error404/src/main/java/com/example/demo/MainController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class MainController {
 
 	@GetMapping("/test1")
-    public String input1() {
-        return "test111/index";
-    }
+	public String input1() {
+		return "test1/index";
+	}
 }
