33c7e416074e7e6e385164a42fdb2c8fdd6f0b53
ut23405 <ut23405@gmail.com>
2021-11-14 18:41:53 +0900

    @SessionAttributes

--- springboot-session-controller/src/main/java/com/example/demo/Syain.java
+++ springboot-session-controller/src/main/java/com/example/demo/Syain.java
@@ -2,7 +2,7 @@ package com.example.demo;
 
 import java.io.Serializable;
 
-public class Syain {
+public class Syain implements Serializable {
 	private static final long serialVersionUID = 1L;
 	private String name;
 
