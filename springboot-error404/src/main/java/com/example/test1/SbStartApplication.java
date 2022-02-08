09a048cfe29e82ff8dd9f7ce6123073d06c3a1f4
ut23405 <ut23405@gmail.com>
2022-02-09 00:17:44 +0900

    404error

--- springboot-error404/src/main/java/com/example/test1/SbStartApplication.java
+++ springboot-error404/src/main/java/com/example/test1/SbStartApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.example.test1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringbootHelloApplication {
+public class SbStartApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootHelloApplication.class, args);
+		SpringApplication.run(SbStartApplication.class, args);
 	}
 
 }
