5651d64f09c1a90957790993f824c20250e85b5b
ut23405 <ut23405@gmail.com>
2022-02-11 21:31:42 +0900

    @SessionAttributes

--- springboot-session-controller/src/main/java/com/example/demo/MainController.java
+++ springboot-session-controller/src/main/java/com/example/demo/MainController.java
@@ -8,27 +8,30 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+/**
+ * http://localhost:8765/test1/
+ */
 @Controller
-@SessionAttributes(value = "testS")
+@SessionAttributes(value = "TestSession")
 public class MainController {
 
-	@ModelAttribute(value = "testS")
+	@ModelAttribute(value = "TestSession")
 	public Syain createSyain() {
 		return new Syain("suzuki");
 	}
 
 	@GetMapping("/test1")
-	public String input1(@ModelAttribute("testS") Syain syain,
+	public String input1(@ModelAttribute("TestSession") Syain syain,
 			Model model) {
 		model.addAttribute(syain);
 		return "test1/index";
 	}
 
 	@PostMapping("/testform")
-	public String output1(@RequestParam String text1, Model model, 
-			@ModelAttribute("testS") Syain syain) {
+	public String output1(@RequestParam String textMemo, Model model, 
+			@ModelAttribute("TestSession") Syain syain) {
 		model.addAttribute(syain);
-		model.addAttribute("moji1", text1);
+		model.addAttribute("memo", textMemo);
 		return "test1/testform";
 	}
 }
